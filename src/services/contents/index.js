090e111df5ce97c94035f5cc939a7c03a5766a2a
Jia Xiong <wing8169@users.noreply.github.com>
2024-01-08 23:58:19 +0800

    Change cheerio to puppeteer

--- src/services/contents/index.js
+++ src/services/contents/index.js
@@ -1,13 +1,26 @@
 import axios from "axios";
-import cheerio from "cheerio";
+import { load } from "cheerio";
+import puppeteer from "puppeteer";
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
 
 export default async (req, res) => {
   try {
     const site = req.query.site;
     console.log("site", site);
-    const resp = await axios.get(site);
-    const $ = cheerio.load(resp.data);
-    const text = $("#content").text();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Navigate the page to a URL
+    await page.goto(site);
+    await delay(1000);
+    const textSelector = await page.waitForSelector("#content");
+    const text = await textSelector?.evaluate((el) => el.textContent);
+
     res.send(
       `<div id="content">${text
         .replace("hｔｔｐs://m•ｈetｕshu.com•cｏm", "")

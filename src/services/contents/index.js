c3dcb9a6896813b248cc13fcd8a4f72d9c929e85
Jia Xiong <wing8169@users.noreply.github.com>
2024-01-09 02:13:20 +0800

    Fix query

--- src/services/contents/index.js
+++ src/services/contents/index.js
@@ -17,12 +17,24 @@ export default async (req, res) => {
 
     // Navigate the page to a URL
     await page.goto(site);
-    await delay(1000);
+    await delay(2000);
     const textSelector = await page.waitForSelector("#content");
     const text = await textSelector?.evaluate((el) => el.textContent);
 
+    const divs = await page.$$("#content div");
+    let rslt = "";
+    for (const div of divs) {
+      const tmpText = await div?.evaluate((el) => el.textContent);
+      const tmpStyle = await div?.evaluate((el) => {
+        const style = window.getComputedStyle(el).getPropertyValue("display");
+        return style;
+      });
+      if (tmpStyle !== "block") continue;
+      rslt += tmpText;
+    }
+
     res.send(
-      `<div id="content">${text
+      `<div id="content">${rslt
         .replace("hｔｔｐs://m•ｈetｕshu.com•cｏm", "")
         .replace("ｍ.hｅtｕsｈu•com•ｃom", "")
         .replace("hetushu.com.com", "")}</div>`
